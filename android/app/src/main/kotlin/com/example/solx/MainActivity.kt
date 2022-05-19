6264ba2e4f87dc4ca6985dc6c8f0b4f1f903eecb
Tanishq Kashyap <tanishqa89@gmail.com>
2022-05-19 10:52:22 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/solx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.solx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/welcome_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:solx/login_screen.dart';
+import 'package:solx/register_screen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "Students' On-Line eXchange",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image(
+                image: NetworkImage(
+                  "https://thumbs.dreamstime"
+                  ".com/b/welcome-hand-lettering-vector-illustration-white"
+                  "-background-isolated-173150776.jpg",
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginScreen(),
+                    ),
+                  );
+                },
+                child: Text("Log-In"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterScreen(),
+                    ),
+                  );
+                },
+                child: Text("Register Now"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
